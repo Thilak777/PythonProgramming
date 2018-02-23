2f23624b694979637169539d05835c7effd818bf
JanaShanmugam <36722610+JanaShanmugam@users.noreply.github.com>
2018-02-23 11:16:53 +0530

    Update leap.py

--- Beginnerlevel/leap.py
+++ Beginnerlevel/leap.py
@@ -1,5 +1,5 @@
 year =int(input("Enter the year :"))
-if (year%4==0 or (year%100==0and year%400==0)):
+if (year%4==0 or (year%100==0 and year%400==0)):
     print ("Leap year")
 else:
     print ("Not a leap year")
