da5b5ee7a408174de8d2dfa867766707fd54c351
JanaShanmugam <36722610+JanaShanmugam@users.noreply.github.com>
2018-02-23 10:28:45 +0530

    Update positive.py

--- Beginnerlevel/positive.py
+++ Beginnerlevel/positive.py
@@ -1,7 +1,9 @@
-a=(input('Enter the value:'))
+a=int(input('Enter the value:'))
 if a>0:
       print "positive"
 elif a<0:
       print "Negative"
 elif a==0:
       print "Zero"
+else:
+      print "Invalid"
